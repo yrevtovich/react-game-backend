95fdba0f50ecb335f2b6b8c2ab7806f09a8ca91b
Yuliya Reutovich <yrevtovich@mail.ru>
2021-03-03 06:34:45 +0300

    refactor: remove comments

--- models/Statistics.js
+++ models/Statistics.js
@@ -1,4 +1,3 @@
-// import { Schema, model, Types, Document } from 'mongoose';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const model = mongoose.model;
@@ -10,7 +9,5 @@ const StatisticsSchema = new Schema({
 }, {
   versionKey: false,
 });
-
-// export default model('Statistics', StatisticsSchema);
 
 module.exports = model('Statistics', StatisticsSchema);
