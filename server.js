61127814935d3d2764c638b22f5e48be329823ce
Yuliya Reutovich <yrevtovich@mail.ru>
2021-03-03 11:23:33 +0300

    refactor: add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
